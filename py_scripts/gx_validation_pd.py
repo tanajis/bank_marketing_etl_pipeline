30e608346ac3035abff398864d0b05baec06420a
Tanaji Sutar <tanajisutar31@gmail.com>
2026-02-18 22:09:06 +0530

    added comments

--- py_scripts/gx_validation_pd.py
+++ py_scripts/gx_validation_pd.py
@@ -5,15 +5,16 @@ from pathlib import Path
 BASE_DIR = Path(__file__).parent.parent
 CSV_FILE_PATH = BASE_DIR / "data" / "bank_marketing_data.csv"
 
+# Entry point
 context = gx.get_context(mode='ephemeral') # ephemeral does not stare metata for future validations
 
 # 1.Create Data Source(Pandas)
 ds = context.data_sources.add_pandas(name="ds_raw_bank_marketing")
 
-# 2.Create Data Asset(Dataframe)
+# 2.Create Data Asset(Dataframe/Table)
 data_asset = ds.add_dataframe_asset(name='da_raw_bank_marketing')
 
-# 3. Batch Defination 
+# 3. Batch Defination (Chunk or parts of the data day, month etc but here we are cosnidering whole data)
 bd = data_asset.add_batch_definition_whole_dataframe("batch_raw_bank_marketing")
 
 # 4. Expectations
@@ -22,13 +23,15 @@ ex_age = gx.expectations.ExpectColumnValuesToBeBetween(column="age",max_value=10
 ex_marital = gx.expectations.ExpectColumnDistinctValuesToBeInSet(column="marital", value_set=["married","single"])
 
 # 5.Create Expecation Suit and Add Expecations
-ex_suit = gx.ExpectationSuite(name="ex_raw_bank_marketing")
+# Expectation Suite is a set of all expecatation on a single dataset
+ex_suit = gx.ExpectationSuite(name="ex_raw_bank_marketing") 
 Expecation_suit_list =context.suites.add(ex_suit)
 Expecation_suit_list.add_expectation(ex_age)
 Expecation_suit_list.add_expectation(ex_marital)
 
 
 # 6.Create Validation Definations
+# Validation Defination defines - which data and which suit?
 
 validation_definition_name = 'Bank Marketing Raw Layer Validation'
 validation_defination_ref = gx.ValidationDefinition(data =bd, suite=Expecation_suit_list, name = validation_definition_name)
@@ -36,6 +39,7 @@ validation_defination = context.validation_definitions.add(validation_defination
 
 
 # 7. Creating checkpoint
+# Checkpoint is a saved validation configuration.Runs validations at a specific moment.Useful for tracking data quality over time.
 
 checkpoint_name ='Bank Marketing Data'
 checkpoint_obj =gx.Checkpoint(name = checkpoint_name,validation_definitions = [validation_defination], actions =[], result_format= "COMPLETE")
